20f7c860af83ac46d51f631b11ac97876794d80e
Kovalyok Oleg <lemuil.dev@gmail.com>
2018-10-27 19:25:56 +0300

    🎨 Implement PP Facade example

--- DesignPatterns.playground/Pages/Facade.xcplaygroundpage/Contents.swift
+++ DesignPatterns.playground/Pages/Facade.xcplaygroundpage/Contents.swift
@@ -9,3 +9,97 @@
  
  Например, в Amazon – покупка с одного клика – как много систем задействовано в операции покупки? И проверка платежной карточки, и проверка Вашего адреса, проверка товара на складе, проверка или возможна доставка данного товара в данную точку мира… В результате очень много действий которые происходят всего по одному клику.
  */
+
+final class Machine {
+    enum State {
+        case notRunning
+        case ready
+        case running
+    }
+    
+    private(set) var state: State = .ready
+    
+    func startMachine() {
+        print("Process starting...")
+        state = .ready
+        state = .running
+        print("Machine is running")
+    }
+    
+    func stopMachine() {
+        state = .notRunning
+        print("Machine stopped")
+    }
+}
+
+
+final class RequestManager {
+    
+    var isConnected: Bool = false
+    
+    func connectToTerminal() {
+        print("Connecting to terminal...")
+        isConnected = true
+    }
+    
+    func disconnectToTerminal() {
+        print("Disconnecting to terminal...")
+        isConnected = false
+    }
+    
+    func getStateRequest(for machine: Machine) -> Machine.State {
+        print("Sending state request...")
+        return machine.state
+    }
+    
+    func sendStartRequest(for machine: Machine) {
+        print("Sending request to start machine")
+        machine.startMachine()
+    }
+    
+    func sendStopRequest(for machine: Machine) {
+        print("Sending request to stop machine")
+        machine.stopMachine()
+    }
+}
+
+
+// Basic implemention without Facade
+
+let machine = Machine()
+let manager = RequestManager()
+
+if !manager.isConnected {
+    manager.connectToTerminal()
+}
+
+if manager.getStateRequest(for: machine) == .ready {
+    print("Machine is ready...")
+    manager.sendStartRequest(for: machine)
+}
+
+
+// Implemention with Facade
+
+protocol Facade {
+    func startMachine()
+}
+
+final class ConcreteFacade: Facade {
+    func startMachine() {
+        let machine = Machine()
+        let manager = RequestManager()
+        
+        if !manager.isConnected {
+            manager.connectToTerminal()
+        }
+        
+        if manager.getStateRequest(for: machine) == .ready {
+            print("Machine is ready...")
+            manager.sendStartRequest(for: machine)
+        }
+    }
+}
+
+let simpleInterfaceToStartMachine = ConcreteFacade()
+simpleInterfaceToStartMachine.startMachine()
